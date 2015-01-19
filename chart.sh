4fa645682aa8eec0af5f287111ce6537cd2f665c
duykhanh <cao.duy.khanh@framgia.com>
2015-01-19 10:56:41 +0700

    Improve command

--- chart.sh
+++ chart.sh
@@ -2,13 +2,50 @@
 function ready_to_test() {
   if [[ $# -eq 0 ]]; then
     command test
+  elif [[ $# -eq 1 && $0 -eq -h || $0 -eq --help ]]; then
+    echo "ready_to_test - Get number of tickets became 'Ready To Test' within period"
+    echo "ARGUMENTS:"
+    echo "  -p          Name of project"
+    echo "  -s          Sprint"
+    echo "  -f          Start date"
+    echo "  -t          End date"
+    echo "EXAMPLE:"
+    echo "  ready_to_test -p rakuten -s 4.1 -f 2014/12/1 -t 2014/12/7"
   else
+    DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
     echo "Enter your Jira's username:"
     read user
     echo "Enter your password:"
     read -s password
     echo "LOADING........"
-    OUTPUT=$(command ruby ~/Desktop/data.rb $1 $2 $3 $4 $user $password)
+    while [[ $# > 1 ]]
+    do
+    key="$1"
+
+    case $key in
+        -p|--project)
+        PROJECT="$2"
+        shift
+        ;;
+        -s|--sprint)
+        SPRINT="$2"
+        shift
+        ;;
+        -f|--from)
+        FROM="$2"
+        shift
+        ;;
+        -t|--to)
+        TO="$2"
+        shift
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+    shift
+    done
+    OUTPUT=$(command ruby ${DIR}/data.rb ${PROJECT} ${SPRINT} ${FROM} ${TO} $user $password)
     IFS=$'*' read -a result <<<"$OUTPUT"
     arr="["
     count=0
